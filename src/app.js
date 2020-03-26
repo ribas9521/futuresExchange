bc5e5362ef556f0f1ab4a6da6861597e6d1ad3bb
ribas9521 <guilherme.rifer@gmail.com>
2020-03-26 16:12:13 -0300

    serverless implementation

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+var serverless = require('serverless-http');
 
-const { router } = require("./api/");
+const { router } = require('./api/');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const {
   authenticateAndConnect,
   loadExchangeMarkets
-} = require("./api/middleware/");
+} = require('./api/middleware/');
 //comentar para serverless
-app.set("port", process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 
-app.use("/api", authenticateAndConnect, loadExchangeMarkets, router);
+app.use('/api', authenticateAndConnect, loadExchangeMarkets, router);
 
 // Configura diretórios de logs
 // if (!fs.existsSync("logs"))
 // fs.mkdirSync("logs");
 
 //comentar para serverless
-app.listen(app.get("port"), () => {
-  console.log("Node app is running on port", app.get("port"));
-});
+// app.listen(app.get("port"), () => {
+//   console.log("Node app is running on port", app.get("port"));
+// });
 
-module.exports = app;
+module.exports = serverless(app);
